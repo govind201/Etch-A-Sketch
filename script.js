b789ae5af33ad37b3a227729afd802f9db1c4dfa
Govind Singh <gs5818475@gmail.com>
2023-05-19 22:26:36 +0530

    refactor changes

--- script.js
+++ script.js
@@ -1,32 +1,58 @@
 const container = document.querySelector("#container");
 const resetBtn = document.querySelector("#reset-btn");
+const MAX_GRID_SIZE = 60;
+const INITIAL_GRID_SIZE = 16;
 
 const createGrid = (size) => {
-    container.innerHTML = "";
-    const gridTotalSize = 960;
-    const cellSize = gridTotalSize / size;
-    for (let i = 0; i < size * size; i++) {
-        const cell = document.createElement("div");
-        cell.classList.add("square");
-        cell.style.width = `${cellSize}px`;
-        cell.style.height = `${cellSize}px`;
-        container.appendChild(cell);
-    }
+  clearGrid();
+  const cellSize = calculateCellSize(size);
+  for (let i = 0; i < size * size; i++) {
+    const cell = createCell(cellSize);
+    container.appendChild(cell);
+  }
+  addHoverEffect();
+};
 
-    const squares = document.querySelectorAll(".square");
-    squares.forEach((square) => {
-        square.addEventListener("mouseover", () => {
-            square.style.backgroundColor = "black";
-        });
+const clearGrid = () => {
+  container.innerHTML = "";
+};
+
+const calculateCellSize = (size) => {
+  const gridTotalSize = 960;
+  return gridTotalSize / size;
+};
+
+const createCell = (cellSize) => {
+  const cell = document.createElement("div");
+  cell.classList.add("square");
+  cell.style.width = `${cellSize}px`;
+  cell.style.height = `${cellSize}px`;
+  return cell;
+};
+
+const addHoverEffect = () => {
+  const squares = document.querySelectorAll(".square");
+  squares.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = "black";
     });
-}
+  });
+};
 
-const initialGridSize = 16;
-createGrid(initialGridSize);
+const promptForNewSize = () => {
+  return prompt("Enter new size (max 60):");
+};
 
-resetBtn.addEventListener("click", () => {
-    let newSize = prompt("Enter new size (max 60):");
-    if (newSize !== null && newSize <= 60) {
-        createGrid(newSize);
-    }
-});
+const isValidSize = (size) => {
+  return size !== null && size <= MAX_GRID_SIZE;
+};
+
+const resetGrid = () => {
+  let newSize = promptForNewSize();
+  if (isValidSize(newSize)) {
+    createGrid(newSize);
+  }
+};
+
+createGrid(INITIAL_GRID_SIZE);
+resetBtn.addEventListener("click", resetGrid);
